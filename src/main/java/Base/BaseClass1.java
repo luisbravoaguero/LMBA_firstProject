d1f3ab149e7aebbbf94045fdf6ce9d63b69b2868
luisbravoaguero <luisbravoaguero@gmail.com>
2022-09-15 11:08:05 -0500

    primer commit en ramab

--- src/main/java/Base/BaseClass1.java
+++ src/main/java/Base/BaseClass1.java
@@ -6,4 +6,12 @@ public class BaseClass1 {
     public BaseClass1(String name){
         this.nombre = name;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
